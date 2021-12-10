10fe11d96efa69f62bea60beffb15c65a120e85f
Paolo Laurenti <laurentipaolo@gmail.com>
2021-12-10 17:36:47 +0100

    complete second exercise of day 2

--- lib/Day2/day_2.ex
+++ lib/Day2/day_2.ex
@@ -14,15 +14,44 @@ defmodule Aoc21.Day2 do
     |> hd()
   end
 
+  def count_position_with_aim!(input) do
+    input
+    |> File.stream!([], :line)
+    |> Stream.map(fn item ->
+      [command, value] = item |> String.trim() |> String.split()
+      {value, _} = Integer.parse(value)
+      {command, value}
+    end)
+    |> Stream.scan(%{horizontal: 0, depth: 0, aim: 0}, fn {command, value}, current_position ->
+      process_command(command, value, current_position)
+    end)
+    |> Enum.reverse()
+    |> hd()
+  end
+
+  defp process_command("forward", value, %{horizontal: horizontal, depth: depth, aim: aim} = position) do
+    %{position | horizontal: horizontal + value, depth: depth + value * aim }
+  end
+
   defp process_command("forward", value, %{horizontal: horizontal} = position) do
     %{position | horizontal: horizontal + value}
+  end
+
+  defp process_command("down", value, %{aim: aim} = position) do
+    %{position | aim: aim + value}
   end
 
   defp process_command("down", value, %{depth: depth} = position) do
     %{position | depth: depth + value}
   end
 
+  defp process_command("up", value, %{aim: aim} = position) do
+    %{position | aim: aim - value}
+  end
+
   defp process_command("up", value, %{depth: depth} = position) do
     %{position | depth: depth - value}
   end
+
+
 end
--- test/Day2/day_2_test.exs
+++ test/Day2/day_2_test.exs
@@ -7,7 +7,15 @@ defmodule Aoc21.Day1Test do
 
     result = Day2.count_position!(input_file_path)
 
-    assert result == %{horizontal: 15, depth: 10} 
+    assert result == %{horizontal: 15, depth: 10}
+  end
+
+  test "submarine position with aim" do
+    input_file_path = "./test/Day2/input_ex_1.txt"
+
+    result = Day2.count_position_with_aim!(input_file_path)
+
+    assert %{horizontal: 15, depth: 60} = result
   end
 
 end
